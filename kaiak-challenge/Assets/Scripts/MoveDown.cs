bdfc77c21d5ae29e80c9fb260e973410d1ced2ce
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-22 22:54:02 -0300

    4.7.2 commit

--- kaiak-challenge/Assets/Scripts/MoveDown.cs
+++ kaiak-challenge/Assets/Scripts/MoveDown.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveDown : MonoBehaviour
 {
     public float speed = 4.0F;
+    public float zDestroy = -25;
     private Rigidbody objRb;
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,9 @@ public class MoveDown : MonoBehaviour
     void Update()
     {
         objRb.AddForce(Vector3.forward * -speed);
+
+        if(transform.position.z < zDestroy){
+            Destroy(gameObject);
+        }
     }
 }

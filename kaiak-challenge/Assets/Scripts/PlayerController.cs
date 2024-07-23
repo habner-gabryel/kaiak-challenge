ff8f553f66cef3d7ccb5e51a5db03b96cf6b53c6
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-22 23:23:49 -0300

    4.7.3 commit

--- kaiak-challenge/Assets/Scripts/PlayerController.cs
+++ kaiak-challenge/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody playerRb;
     public float horizontalInput;
     private float speed = 15F;
     private float xRange = 15F;
 
+    public int pontos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,17 @@ public class PlayerController : MonoBehaviour
             } else {
                 transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
             }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Pedra")){
+            Destroy(gameObject);
+        } else if(collision.gameObject.CompareTag("Rampa")){
+            playerRb.AddForce(Vector3.up * speed * collision.transform.position.y, ForceMode.Impulse);
+            pontos += 5;
+        } else if(collision.gameObject.CompareTag("Bandeira")){
+            pontos += 2;
         }
     }
 }
